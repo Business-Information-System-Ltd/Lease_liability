2eb5da0099bddbdaed5de293a339cf47adaa3af6
hsu-coder <hsuthazin255781@gmail.com>
2026-02-18 10:17:29 +0630

    code

---
+++ lease_liability/lease_liability/wsgi.py
@@ -0,0 +1,27 @@
+"""
+WSGI config for lease_liability project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lease_liability.settings')
+
+application = get_wsgi_application()
+# import os
+# import sys
+
+# from django.core.wsgi import get_wsgi_application
+
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.append(BASE_DIR)
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lease_liability.settings')
+
+# application = get_wsgi_application()
